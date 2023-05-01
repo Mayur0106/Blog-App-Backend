306023cf449b10ad94d4f4f129b0425d5cb761b1
Mayur Shinde <97580421+Mayur0106@users.noreply.github.com>
2023-05-01 17:46:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -20,8 +20,8 @@ app.use(express.json());
 app.use(cookieParser());
 app.use('/uploads', express.static(__dirname + '/uploads'));
 
-mongoose.connect('mongodb+srv://blog-pages:X54D8pZFqTJnx6h4@cluster0.4dpwnxm.mongodb.net/?retryWrites=true&w=majority');
-const PORT = process.env.PORT || 5001;
+// mongoose.connect('mongodb+srv://blog-pages:X54D8pZFqTJnx6h4@cluster0.4dpwnxm.mongodb.net/?retryWrites=true&w=majority');
+const PORT = process.env.PORT || 4000;
 
 app.post('/register', async (req,res) => {
   const {username,password} = req.body;
